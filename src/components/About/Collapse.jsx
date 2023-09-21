faf8caa5771240fe37518734e7743f45ff367b75
alice G <geoffroy.aurelien@outlook.fr>
2023-09-21 15:53:50 +0200

    collapsible

--- src/components/About/Collapse.jsx
+++ src/components/About/Collapse.jsx
@@ -1,28 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import '../../style/About/Collapse.css';
 import Chevron from '../../assets/Vector.png';
-import Button from './Button';
-
-//-----On importe les hooks-----
-import{useState} from 'react';
 
 //-----Gestion de l'affichage des informations-----
 function Collapse ({title, description}){
-//-----On déclare une variable d'état (Open)-----
-//-----setOpen est la fonction de cette variable(initialement fausse)-----
+//-----On déclare une variable d'état (Open) en appelent le hook useState-----
+//-----Paire de valeur avec Open et la fonction setOpen-----
+//-----L'état useState est fermé par défaut-----
     const [Open, setOpen] = useState(false);
-    const OpenDirection = () => {
-        setOpen((Open) => !Open);
-    };
 
+    const handleOpen = () => {
+        setOpen(!Open);
+    }
+//-----On englobe la div à basculer dans des accolades pour utiliser JS-----
+//----- L' opérateur && est là pour déterminer la partie vraie-----
     return (
         <div className="info">
-            <div className="infos" onClick={OpenDirection}>
-                <h1 className="title"  onClick={() => setOpen(!Open)}>{title}
-                <img className="chevron" src={Chevron} alt="chevron"/>
+            <div className="infos">
+                <h1 className="title">{title}
+                    <img src={Chevron} className="chevron" onClick={handleOpen} alt="chevron"/>
                 </h1>
             </div>
-            <div className="info-description">{description}</div>
+            {Open &&
+            <div className="info-description">
+                <p>{description}</p>
+            </div>}
         </div>
     )
 }
--- src/pages/Accommodation/Accommodation.jsx
+++ src/pages/Accommodation/Accommodation.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import '../../style/Accommodation/Accommodation.css';
 import Header from '../../components/Header';
+import Slideshow from '../../components/Accommodation/Slideshow';
+import Main from '../../components/Accommodation/Main';
 import Footer from '../../components/Footer';
 
 function Accommodation() {
     return (
         <>
             <Header />
-
+            <Slideshow />
+            <Main />
             <Footer />
         </>
     )
